484a3181ffbdc5c0c50bd525416cd07e1cb42d04
Nagarjun <w3.nagarjuna@gmail.com>
2022-05-09 12:42:26 +0530

    Basic pages setup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <p>{{ message }}</p>
+  <router-view />
 </template>
 
 <script>
-import { fetchMessage } from '@/services/fetchers';
-
 export default {
-  data() {
-    return {
-      message: '',
-    };
-  },
-  async created() {
-    try {
-      this.message = await fetchMessage();
-    } catch (error) {
-      this.message = 'server error :(';
-    }
-  },
+  name: 'App',
 };
 </script>
+
+<style lang="scss">
+@import './assets/styles.scss';
+</style>
---
+++ src/views/MockServiceWorkerView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Mock Service Worker</div>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+import { fetchMessage } from '@/services/fetchers';
+
+export default {
+  name: 'MockServiceWorkerView',
+  components: {},
+  data() {
+    return {
+      message: '',
+    };
+  },
+  async created() {
+    try {
+      this.message = await fetchMessage();
+    } catch (error) {
+      this.message = 'server error :(';
+    }
+  },
+};
+</script>
